3a88d4fc7a106c72fdaf5a7c100e4d9babacebe9
Gilbert Torchon <gilbert@noukod.com>
2018-08-30 16:32:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,10 @@
 
 
 def fizzbuzz(number)
- if number % 3 == 0 # if the number int is divisible by 3
+ if (number % 3 == 0) && (number % 5 == 0)
+    "FizzBuzz"
+ elsif number % 3 == 0 # if the number int is divisible by 3
    "Fizz" # Go fizz
  elsif number % 5 == 0
    "Buzz"
- elsif (number % 3 == 0) && (number % 5 == 0)
-   "FizzBuzz"
- end
 end
